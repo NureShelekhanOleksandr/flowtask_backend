f79ac59cb365ef3cbd3ea17e2bbc95a5060c367b
NureShelekhanOleksandr <sashashelehan@gmail.com>
2025-06-12 02:57:34 +0300

    update app

--- app/database.py
+++ app/database.py
@@ -11,6 +11,7 @@ DATABASE_URL = os.getenv(
     "mssql+pyodbc://azureuser:e28haY-D#HrAd_r@flow-task-sqlserver.database.windows.net:1433/flow-task-adb?driver=ODBC+Driver+18+for+SQL+Server",
 )
 
+
 # Create engine with Azure SQL specific settings
 engine = create_engine(
     DATABASE_URL,
